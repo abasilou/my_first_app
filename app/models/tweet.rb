d9e81942c5dc732c713e2126ea19f8d9d4fcbeaa
Basil Al Bader <basilalbader@gmail.com>
2014-10-19 12:28:34 -0400

    tweets belong_to users

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,5 @@
 class Tweet < ActiveRecord::Base
 
 		validates :conten, length: { maximum: 140 } 
+		belongs_to :user
 end 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018212525) do
+ActiveRecord::Schema.define(version: 20141019160556) do
 
   create_table "dougs", force: true do |t|
     t.datetime "created_at"
@@ -22,7 +22,10 @@ ActiveRecord::Schema.define(version: 20141018212525) do
     t.string   "conten"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "tweets", ["user_id"], name: "index_tweets_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
